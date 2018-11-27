1df86948c320cbb207b3f91dd47ba0e49297708d
chan <986632543@qq.com>
2018-11-27 22:29:27 +0800

    Initial commit

---
+++ module2/src/main/java/chen/App.java
@@ -0,0 +1,17 @@
+package chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello 2World!" );
+        int a=1;
+        int b=2;
+        int c=a+b;
+    }
+}
